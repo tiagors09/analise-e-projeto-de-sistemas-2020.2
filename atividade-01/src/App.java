c266e00c0dbaec92d0f8c4ccd47e7cf9c83668b0
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2020-12-06 11:47:35 -0300

    adicionado shell de interação

--- atividade-01/src/App.java
+++ atividade-01/src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        boolean shellAtivo = true;
+        String linha;
+        String[] comandos;
+        RevendaComArray rev = new RevendaComArray();
+
+        //Inicialização do shell
+        while(shellAtivo) {
+            //pegando comandos
+            linha = in.nextLine();
+            //separando comandos  
+            comandos = linha.split(" ");
+            
+            switch (comandos[0]) {
+                //inicializando sistema de vendas
+                case "init":
+                    rev = new RevendaComArray(Integer.parseInt(comandos[1]));
+                    break;
+                //inserindo produtos  
+                case "in":
+                    if(comandos.length-1 == 2)
+                        rev.inserirProduto(new Produto(Integer.parseInt(comandos[1]), comandos[2]));
+                    else if(comandos.length-1 == 5)
+                        rev.inserirProduto(new Produto(Integer.parseInt(comandos[1]), comandos[2], Double.parseDouble(comandos[3]), Double.parseDouble(comandos[4]), Double.parseDouble(comandos[5])));
+                    else if(comandos.length-1 == 6)
+                        rev.inserirProduto(new Produto(Integer.parseInt(comandos[1]), comandos[2], Double.parseDouble(comandos[3]), Double.parseDouble(comandos[4]), Double.parseDouble(comandos[5])));
+                    break;
+                //comprando produto pra revender 
+                case "comp": 
+                    rev.comprar(Integer.parseInt(comandos[1]), Integer.parseInt(comandos[2]));
+                    break;
+                default:
+                    break;
+            }
+        }
+        
     }
 }
