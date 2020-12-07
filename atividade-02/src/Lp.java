e5f613e9357d6fdb2a18a2aee1fb7cd306e7aa02
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2020-12-07 09:30:43 -0300

    adicionado classe Lp

---
+++ atividade-02/src/Lp.java
@@ -0,0 +1,9 @@
+public class Lp extends Musica {
+    private boolean raro;
+    
+    @Override
+    protected double calcularDiaria() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
