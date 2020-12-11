de55a4f3c521c0afa0c6d8c8ba773c4e3b48ad17
Tiago Rodrigues <tiagorodriguessousa9@gmail.com>
2020-12-11 09:15:40 -0300

    modificado classes

--- atividade-02/src/entidades/atores/OperadorSistema.java
+++ atividade-02/src/entidades/atores/OperadorSistema.java
@@ -11,4 +11,8 @@ public class OperadorSistema extends Funcionario {
      * Procurar clientes por matricula.
      */
 
+    //Construtor
+    public OperadorSistema(String nome, int matricula, String login, String senha) {
+        super(nome, matricula, login, senha);
+    }
 }
--- atividade-02/src/entidades/produtos/Vhs.java
+++ atividade-02/src/entidades/produtos/Vhs.java
@@ -5,6 +5,10 @@ import entidades.produtos.abstratos.Filme;
 public class Vhs extends Filme {
     private boolean cores;
     
+    public Vhs(int anoLancamento, int duracao) {
+        super(anoLancamento, duracao);
+    }
+
     @Override
     public double calcularDiaria() {
         // TODO Auto-generated method stub
